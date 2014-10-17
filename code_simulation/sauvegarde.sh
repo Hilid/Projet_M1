1d046fe6019b47ebaf50e29905bbba618085b7dc
Hilid <thomalechat@hotmail.fr>
2014-10-17 20:30:58 +0200

    ajout des commandes de sauvegarde de R et T

--- code_simulation/sauvegarde.sh
+++ code_simulation/sauvegarde.sh
@@ -8,7 +8,16 @@ then
 		cp config.txt log.txt   													   #sauvegarde de la configuration du réseau
 		touch $PPID.m                                             					     #creation d'un script .m temporaire
 		cat main.m > $PPID.m                                   								        #reprise du code complet
-		echo "for (i=1:$2) saveas(i,['figure'  num2str(i) '.png'],'png'); end" >> $PPID.m            #ajout des lignes de print
+		echo "for (i=1:$2) saveas(i,['figure'  num2str(i) '.png'],'png'); end" >> $PPID.m            #ajout des lignes de print en png
+
+#======================================================================================================================================================
+#Sauvegarde des coefficients de reflexions et transmission
+#        echo "coll1 = ones(N,1); coll2 = ones(N,1); coll3 = ones(N,1); coll4 = ones(N,1);">>$PPID.m
+#		echo "for (i =1:N) coll1(i) = real(R)(1,1,i); coll2(i) = imag(R)(1,1,i); coll3(i) = real(T)(1,1,i); coll4(i) = imag(T)(1,1,i); end" >> $PPID.m
+#		echo "T_sauvegarde = [f' coll3 coll4]; R_sauvegarde = [f' coll1 coll2]; " >> $PPID.m
+#		echo "save('R.txt', 'R_sauvegarde'); save('T.txt', 'T_sauvegarde');" >>$PPID.m 
+#====================================================================================================================================================
+	
 		octave --silent --no-window-system $PPID.m { >> log.txt }   					#éxecution du script modifié et récupération du résultat
 
 		mkdir $1																		#creation du dossier de reception
@@ -19,7 +28,13 @@ then
 		done
 
 		mv $1 archives_figures															#rangement du dossier de reception
-		rm $PPID.m 																		#suppression du fichier temporaire
+		rm $PPID.m 		
+
+#=====================================================================================================================================================
+#		mv R.txt $1
+#		mv T.txt $1																
+#=====================================================================================================================================================
+
 	fi                              						                 
 else
   echo "ERREUR: Il y a un probleme d'argument => mettre un mot et le nb de figure"			#erreur pour argument différent de 2
